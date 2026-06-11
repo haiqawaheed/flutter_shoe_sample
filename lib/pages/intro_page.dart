4100b81a651a902b7a7f172220a109b08ca6b0bb
haiqawaheed <haiqawaheed667@gmail.com>
2026-06-11 12:32:42 +0500

    change the image name to nike

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -9,38 +9,70 @@ class IntroPage extends StatelessWidget {
       backgroundColor: Colors.grey[300],
       body: Center(
         child: Center(
-          child: Column(
-            children: [
-              // logo
-              Padding(
-                padding: const EdgeInsets.all(25.0),
-                child: Image.asset(
-                  'lib/images/nike.png',
-                  height: 240,
-                ),
-              ), // Image.asset
-                const SizedBox(height: 48),
-              // title
-               const Text(  'Just Do It',
-               style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-               ),
-               ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal:25.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // logo
+                Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: Image.asset(
+                    'lib/images/nike.png',
+                    height: 240,
+                  ),
+                ), // Image.asset
+                  const SizedBox(height: 48),
+                // title
+                 const Text(  'Just Do It',
+                 style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                 ),
+                 ),
+            
+                 const SizedBox(height: 24),
+            
+                   // sub title
+                  const Text('Brand new sneakers and custom kicks made with premium quality',
+                  style: TextStyle(
+                   fontSize: 16,
+                   color: Colors.grey,
+                    ), // TextStyle
+                    textAlign: TextAlign.center,
+                    ), 
+                  
+                  const SizedBox(height: 48),
+                // start now button
+              
+GestureDetector(
+   onTap: () => Navigator.push(
+    context,
+   MaterialPageRoute(
+    builder: (context) => HomePage(),
+   ),
+),
+  child: Container(
+    decoration: BoxDecoration(
+      color: Colors.grey[900],
+      borderRadius: BorderRadius.circular(12),
+    ), 
+    padding: const EdgeInsets.all(25),
+    child: const Center(
+      child: Text(
+        'Shop Now',
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 16,
+        ), //
+      ),
+    ),
+  ),
+)
 
-               const SizedBox(height: 24),
-
-                 // sub title
-                const Text('Brand new sneakers and custom kicks made with premium quality',
-                style: TextStyle(
-                 fontSize: 16,
-                 color: Colors.grey,
-                  ), // TextStyle
-                  textAlign: TextAlign.center,
-                  ), 
-
-              // start now button
-            ],
+              ],
+            ),
           ),
         ), // Column
       ), // Center

240eab3dd8e8789d1eb8523896a2bb0dddf55616
haiqawaheed <haiqawaheed667@gmail.com>
2026-06-15 19:06:15 +0500

    added drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shoe_store/pages/shop_page.dart';
-
 import '../components/bottom_nav_bar.dart';
 import 'cart_page.dart';
 
@@ -10,36 +9,48 @@ class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
 }
+
 class _HomePageState extends State<HomePage> {
-  // this selected index is to control the bottom nav bar
+  // Selected index to control the bottom nav bar
   int _selectedIndex = 0;
 
-  // this method will update our selected index
-  // when the user taps on the bottom bar
+  // This method will update our selected index
   void navigateBottomBar(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
 
-  // pages to display
+  // Pages to display
   final List<Widget> _pages = [
-    // shop page
-  
     const ShopPage(),
-    // cart page
     const CartPage(),
   ];
-}
 
- @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       bottomNavigationBar: MyBottomNavBar(
-        onTabChange: (index) => navigateBottomBar(index), 
+        onTabChange: (index) => navigateBottomBar(index),
       ),
+       appBar: AppBar(
+    backgroundColor: Colors.transparent,
+    elevation: 0,
+    leading: Builder(
+      builder: (context) => IconButton(
+        icon: const Icon(
+          Icons.menu,
+          color: Colors.black,
+        ), 
+        onPressed: () {
+          Scaffold.of(context).openDrawer();
+        },
+      ), 
+    ), 
+  ), 
+  drawer: const Drawer(),
       body: _pages[_selectedIndex],
     );
   }
-
+}

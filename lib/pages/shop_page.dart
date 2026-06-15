f9f18c7d7eb19c8c208f4f3538032eef489e59e7
haiqawaheed <haiqawaheed667@gmail.com>
2026-06-15 23:51:49 +0500

    design shop page layout with search and hot picks

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -10,6 +10,72 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('shop'),);
+    return Column(
+      children: [
+        // search bar
+Container(
+    padding: const EdgeInsets.all(12),
+    margin: const EdgeInsets.symmetric(horizontal: 25),
+    decoration: BoxDecoration(color: Colors.grey[200],borderRadius:BorderRadius.circular(8)),
+    child: const Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: const [
+        Text(
+          'Search',
+        style:TestStyle(color:Colors.grey),),
+        Icon(
+          Icons.search,
+          color:Colors.grey),
+      ],
+    ), // Row
+  ), // Container
+        // message
+      
+           Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25.0),
+            child: Text(
+             'everyone flies.. some fly longer than others',
+              style: TextStyle(color: Colors.grey[600]),
+             ), 
+             ), 
+
+        // hot picks
+// hot picks
+Padding(
+  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+  child: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    crossAxisAlignment: CrossAxisAlignment.end,
+    children: const [
+      Text(
+        'Hot Picks 🔥',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 24,
+        ), // TextStyle
+      ), // Text
+      Text(
+        'See all',
+        style: TextStyle(
+          fontWeight: FontWeight.bold, 
+          color: Colors.blue,
+        ),
+      ),
+    ],
+  ), 
+),
+const SizedBox(height: 10),
+
+Expanded(
+  child: ListView.builder(
+    itemBuilder: (context, index) {
+      return ShoeTile();
+    }, 
+  ),
+ )
+
+      ],
+
+    );
   }
 }

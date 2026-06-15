3e3327be1b7568581f8c59f57a867b578a709e12
haiqawaheed <haiqawaheed667@gmail.com>
2026-06-15 18:22:08 +0500

    implement HomePage state management and page switching list

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,8 +8,28 @@ class HomePage extends StatefulWidget {
   @override
   State<HomePage> createState() => _HomePageState();
 }
-
 class _HomePageState extends State<HomePage> {
+  // this selected index is to control the bottom nav bar
+  int _selectedIndex = 0;
+
+  // this method will update our selected index
+  // when the user taps on the bottom bar
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  // pages to display
+  final List<Widget> _pages = [
+    // shop page
+    const ShopPage(),
+
+    // cart page
+    const CartPage(),
+  ];
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,4 +37,4 @@ class _HomePageState extends State<HomePage> {
     bottomNavigationBar: MyBottomNavBar(),
     );
   }
-}
+

5cb77522e5130c67833d896a3005a725c088b9bd
haiqawaheed <haiqawaheed667@gmail.com>
2026-06-16 07:47:08 +0500

    project done

--- lib/components/cart_item.dart
+++ lib/components/cart_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../models/shoe.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_shoe_store/models/cart.dart';
 
 class CartItem extends StatefulWidget {
   final Shoe shoe;
@@ -12,27 +14,27 @@ class CartItem extends StatefulWidget {
   State<CartItem> createState() => _CartItemState();
 }
 class _CartItemState extends State<CartItem> {
-  //remove item from cart
-  void removeItemfromCart(){
+  // remove item from cart
+  void removeItemFromCart() {
     Provider.of<Cart>(context, listen: false).removeItemFromCart(widget.shoe);
-
   }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         color: Colors.grey[100],
         borderRadius: BorderRadius.circular(8),
-      ), // BoxDecoration
+      ),
       margin: const EdgeInsets.only(bottom: 10),
       child: ListTile(
         leading: Image.asset(widget.shoe.imagePath),
         title: Text(widget.shoe.name),
         subtitle: Text(widget.shoe.price),
         trailing: IconButton(
-  icon: const Icon(Icons.delete),
-  onPressed: removeItemFromCart,
-),
+          icon: const Icon(Icons.delete),
+          onPressed: removeItemFromCart,
+        ), // IconButton
 
       ),
     ); 
